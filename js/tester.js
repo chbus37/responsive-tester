aaa997efb05c0c52519eb483f2f73f9b36bf1e3d
Christian Bustos <christianbus37@gmail.com>
2024-05-21 16:34:59 -0300

    agregado boton close

--- js/tester.js
+++ js/tester.js
@@ -1,13 +1,23 @@
 const d = document;
 
-export default function responsiveTester(form){
-    const $form = d.getElementById(form);
-    let tester;
+export default function responsiveTester(form) {
+  const $form = d.getElementById(form);
+  let tester;
 
-    d.addEventListener("submit" , (e) => {
-        if(e.target === $form){
-            e.preventDefault();
-            tester = window.open($form.direction.value,"tester",`innerWidth=${$form.width.value}, innerHeight=${$form.height.value}`);
-        }
-    })
+  d.addEventListener("submit", (e) => {
+    if (e.target === $form) {
+      e.preventDefault();
+      tester = window.open(
+        $form.direction.value,
+        "tester",
+        `innerWidth=${$form.width.value}, innerHeight=${$form.height.value}`
+      );
+    }
+  });
+
+  d.addEventListener("click", e => {
+    if(e.target === $form.close){
+        tester.close();
+    }
+  });
 }
