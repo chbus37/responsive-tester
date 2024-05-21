c6e6723eee0bf0f3e92d6330417f31edc1184e96
Christian Bustos <christianbus37@gmail.com>
2024-05-21 16:31:16 -0300

    primer commit

---
+++ js/index_dom.js
@@ -0,0 +1,7 @@
+import responsiveTester from "./tester.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded" , (e) => {
+    responsiveTester("responsive-tester");
+});
---
+++ js/tester.js
@@ -0,0 +1,13 @@
+const d = document;
+
+export default function responsiveTester(form){
+    const $form = d.getElementById(form);
+    let tester;
+
+    d.addEventListener("submit" , (e) => {
+        if(e.target === $form){
+            e.preventDefault();
+            tester = window.open($form.direction.value,"tester",`innerWidth=${$form.width.value}, innerHeight=${$form.height.value}`);
+        }
+    })
+}
